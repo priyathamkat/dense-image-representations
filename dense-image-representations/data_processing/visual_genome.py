06a7193d994311852089deca8dc503726b16240b
Neha Kalibhat <neha.kalibhat@gmail.com>
2023-12-04 18:45:57 -0500

    add t5 embedding to dataset. Basic training code

--- dense-image-representations/data_processing/visual_genome.py
+++ dense-image-representations/data_processing/visual_genome.py
@@ -10,18 +10,22 @@ sys.path.append('/cmlscratch/nehamk/segment-anything')
 from segment_anything import sam_model_registry
 from segment_anything.utils.transforms import ResizeLongestSide
 
+from transformers import AutoTokenizer, T5EncoderModel
 import pdb
 
-class VisualGenomeRelationshipWithSAM(Dataset):
-    def __init__(self, root_dir, sam_model, transform=None):
+class VisualGenomeRelationshipWithSAMAndT5(Dataset):
+    def __init__(self, root_dir, sam_model, t5_tokenizer, t5_encoder, transform=None):
         self.root_dir = root_dir
         self.transform = transform
         # check if sam relationship emb df is already saved
-        if os.path.exists(os.path.join(root_dir, 'relationships_bbox_sam_emb.json')):
-            self.data = pd.read_json(os.path.join(root_dir, 'relationships_bbox_sam_emb.json'))
+        if os.path.exists(os.path.join(root_dir, 'relationships_dataset_with_emb.json')):
+            self.data = pd.read_json(os.path.join(root_dir, 'relationships_dataset_with_emb.json'))
         else:
             self.sam_prompt_encoder = sam_model.prompt_encoder
             self.sam_transform = ResizeLongestSide(sam_model.image_encoder.img_size)
+            self.t5_tokenizer = t5_tokenizer
+            self.t5_encoder = t5_encoder
+
             self.relationship_df = pd.read_json(os.path.join(root_dir, 'relationships.json'))
             self.image_data_df = pd.read_json(os.path.join(root_dir, 'image_data.json'))
             self.image_folder = os.path.join(root_dir, 'VG_100K')
@@ -51,18 +55,20 @@ class VisualGenomeRelationshipWithSAM(Dataset):
                 # The first emb in sparse_embeddings is torch.empty 
                 object_bbox_emb = self.sam_prompt_encoder(boxes = torch.Tensor(self.sam_transform.apply_boxes(object_bbox, image_size)), points = None, masks = None)[0][0,0]
                 subject_bbox_emb = self.sam_prompt_encoder(boxes = torch.Tensor(self.sam_transform.apply_boxes(subject_bbox, image_size)), points = None, masks = None)[0][0,0]
+                predicate_emb = self.t5_encoder(input_ids=self.t5_tokenizer(relationship['predicate'], return_tensors="pt").input_ids).last_hidden_state[0,0]
 
                 self.data.append({
                     'image_id': row['image_id'],
                     'image_path': os.path.join(self.image_folder, str(row['image_id']) + '.jpg'),
                     'predicate': relationship['predicate'],
+                    'predicate_emb': predicate_emb.detach().numpy(),
                     'object': relationship['object'],
                     'subject': relationship['subject'],
                     'object_bbox_sam_emb': object_bbox_emb.detach().numpy(),
                     'subject_bbox_sam_emb': subject_bbox_emb.detach().numpy(),
                 })
         self.data = pd.DataFrame(self.data)
-        self.data.to_json(os.path.join(self.root_dir, 'relationships_bbox_sam_emb.json'))
+        self.data.to_json(os.path.join(self.root_dir, 'relationships_dataset_with_emb.json'))
 
 
     def __len__(self):
@@ -74,9 +80,11 @@ class VisualGenomeRelationshipWithSAM(Dataset):
         img = img.convert('RGB')
         if self.transform:
             img = self.transform(img)
-        return img, row['object_bbox_sam_emb'], row['subject_bbox_sam_emb'], row['predicate']
+        return img, torch.Tensor(row['object_bbox_sam_emb']), torch.Tensor(row['subject_bbox_sam_emb']), torch.Tensor(row['predicate_emb'])
 
 
 if __name__=='__main__':
-    sam = sam_model_registry['vit_b'](checkpoint='sam_vit_b_01ec64.pth')
-    dataset = VisualGenomeRelationshipWithSAM(root_dir='/cmlscratch/nehamk/datasets/visual_genome', sam_model=sam)
+    sam = sam_model_registry['vit_b'](checkpoint='/cmlscratch/nehamk/dense-image-representations/dense-image-representations/sam_vit_b_01ec64.pth')
+    tokenizer = AutoTokenizer.from_pretrained("t5-small")
+    encoder = T5EncoderModel.from_pretrained("t5-small")
+    dataset = VisualGenomeRelationshipWithSAMAndT5(root_dir='/cmlscratch/nehamk/datasets/visual_genome', sam_model=sam, t5_tokenizer=tokenizer, t5_encoder=encoder)
--- dense-image-representations/train_relationship_extract.py
+++ dense-image-representations/train_relationship_extract.py
@@ -1,3 +1,4 @@
+import argparse
 from torchvision import transforms
 from torch.utils.data import DataLoader
 
@@ -6,6 +7,35 @@ import sys
 sys.path.append('/cmlscratch/nehamk/segment-anything')
 from segment_anything import sam_model_registry
 
-sam = sam_model_registry['vit_b'](checkpoint='../sam_vit_b_01ec64.pth')
-dataset = VisualGenomeRelationshipWithSAM(root_dir='/cmlscratch/nehamk/datasets/visual_genome', sam_model=sam)
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--batch_size", type=int, default=256)
+parser.add_argument("--num_workers", type=int, default=16)
+
+args = parser.parse_args()
+
+device = torch.device('cuda')
+sam = sam_model_registry['vit_b'](checkpoint='../sam_vit_b_01ec64.pth')
+t5_tokenizer = AutoTokenizer.from_pretrained('t5-small')
+t5_model = T5EncoderModel.from_pretrained('t5-small')
+
+transform = transforms.Compose([
+    transforms.Resize((1024, 1024)),
+    transforms.ToTensor()
+])
+
+dataset = VisualGenomeRelationshipWithSAM(root_dir='/cmlscratch/nehamk/datasets/visual_genome', sam_model=sam, t5_tokenizer=t5_tokenizer, t5_encoder=t5_model, transform=transform)
+
+dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
+
+sam_image_encoder = sam.image_encoder.to(device)
+
+for i, batch in enumerate(dataloader):
+    img, object_bbox_sam_emb, subject_bbox_sam_emb, predicate_emb = batch
+    img = img.to(device)
+    object_bbox_sam_emb = object_bbox_sam_emb.to(device)
+    subject_bbox_sam_emb = subject_bbox_sam_emb.to(device)
+    predicate_emb = predicate_emb.to(device)
+
+    # train relationship extractor
+    
