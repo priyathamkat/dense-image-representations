129b27285f2570a0d26494ffba15ca4a046ad075
Neha Kalibhat <neha.kalibhat@gmail.com>
2024-01-19 15:05:56 -0500

    adding seem_module into project

---
+++ dense-image-representations/run_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=comp
+#SBATCH --output launch.log
+#SBATCH --error launch.log
+#SBATCH --time=10:00:00
+#SBATCH --gres=gpu:rtxa6000:1
+#SBATCH --qos=scavenger
+#SBATCH --account=scavenger
+#SBATCH --partition=scavenger
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=32G
+
+python3 compute_graph_reps.py \
+--visual_nodes_save_path coco_visual_nodes \
+--batch_size 1 \
