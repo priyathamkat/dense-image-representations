4e78c8eac424c7a96ffe8f1354bf68af4dc7c49c
priyathamkat <priyathamkat@gmail.com>
2024-04-25 18:23:20 -0400

    Use image embedding

--- dense-image-representations/modules.py
+++ dense-image-representations/modules.py
@@ -9,14 +9,18 @@ from torch.nn import TransformerEncoder, TransformerEncoderLayer
 
 
 class MLP(nn.Module):
-    def __init__(self, dim, num_layers=3) -> None:
+    def __init__(self, in_features, out_features, num_layers=3) -> None:
         super().__init__()
-        self.layers = nn.ModuleList([nn.Linear(dim, dim) for _ in range(num_layers)])
+        self.layers = nn.ModuleList(
+            [nn.Conv1d(in_features, in_features, 1) for _ in range(num_layers - 1)]
+        )
+        self.final = nn.Conv1d(in_features, out_features, 1)
 
     def forward(self, x):
         for layer in self.layers:
             x = layer(x)
             x = F.relu(x)
+        x = self.final(x)
         return x
 
 
@@ -58,6 +62,11 @@ class ProjectionHead(nn.Module):
         return x
 
 
+EDGES = "edges"
+IMAGE_EMBEDDING = "image_embedding"
+NODES = "nodes"
+
+
 class VisionLanguageEncoder(nn.Module):
     def __init__(
         self,
@@ -66,8 +75,9 @@ class VisionLanguageEncoder(nn.Module):
         transformer_width: int,
         transformer_heads: int,
         transformer_layers: int,
+        image_embedding_size: int,
         context_length: int = 77,
-        embed_edges: bool = False,
+        preembed_nodes: bool = False,
         #  vocab_size: int = 49408,
     ):
         super().__init__()
@@ -77,7 +87,9 @@ class VisionLanguageEncoder(nn.Module):
 
         self.image_transformer = TransformerEncoder(
             encoder_layer=TransformerEncoderLayer(
-                d_model=transformer_width, nhead=transformer_heads
+                d_model=transformer_width,
+                nhead=transformer_heads,
+                batch_first=True,
             ),
             num_layers=transformer_layers,
         )
@@ -86,9 +98,16 @@ class VisionLanguageEncoder(nn.Module):
 
         # self.vocab_size = vocab_size
         # self.token_embedding = nn.Embedding(vocab_size, transformer_width)
-        self.positional_embedding = nn.Parameter(
-            torch.empty(self.context_length, transformer_width)
+        self.positional_embeddings = nn.ParameterDict(
+            {
+                EDGES: nn.Parameter(torch.empty((transformer_width,))),
+                NODES: nn.Parameter(torch.empty((transformer_width,))),
+            }
         )
+        if not preembed_nodes:
+            self.positional_embeddings[IMAGE_EMBEDDING] = nn.Parameter(
+                torch.empty((transformer_width,))
+            )
         self.ln_final = nn.LayerNorm(transformer_width)
 
         # self.text_projection = nn.Parameter(torch.empty(transformer_width, embed_dim))
@@ -101,15 +120,20 @@ class VisionLanguageEncoder(nn.Module):
             embedding_dim=embed_dim, projection_dim=projection_dim, dropout=0.1
         )
 
-        self.edge_embedding = None
-        if embed_edges:
-            self.edge_embedding = MLP(transformer_width)
+        self.preembed_nodes = preembed_nodes
+        if self.preembed_nodes:
+            self.image_embedder = MLP(
+                transformer_width + image_embedding_size, transformer_width
+            )
+        else:
+            self.image_embedder = MLP(image_embedding_size, transformer_width)
 
         self.initialize_parameters()
 
     def initialize_parameters(self):
         # nn.init.normal_(self.token_embedding.weight, std=0.02)
-        nn.init.normal_(self.positional_embedding, std=0.01)
+        for _, v in self.positional_embeddings.items():
+            nn.init.normal_(v, std=0.01)
 
         # proj_std = (self.transformer.width ** -0.5) * ((2 * self.transformer.layers) ** -0.5)
         # attn_std = self.transformer.width ** -0.5
@@ -123,18 +147,65 @@ class VisionLanguageEncoder(nn.Module):
         # if self.text_projection is not None:
         #     nn.init.normal_(self.text_projection, std=self.transformer.width ** -0.5)
 
-    def encode_image(self, image_tokens, image_attention_mask, num_nodes: int = None):
-        # x = self.token_embedding(image_tokens)  # [batch_size, n_ctx, d_model]
-        if self.edge_embedding and num_nodes is not None:
-            image_tokens["edges"] = self.edge_embedding(image_tokens["edges"])
+    def encode_image(
+        self,
+        tokens,
+        image_embedding,
+        num_non_pad_tokens,
+        num_nodes,
+        image_attention_mask,
+    ):
+        """Expects image_tokens to be a dictionary with three keys: 'edges', 'nodes' and 'image_embedding'.
+        The shapes of the values must be the following:
 
-        x = torch.cat([image_tokens["edges"], image_tokens["nodes"]], dim=0)
-        # x = x + self.positional_embedding  # TODO: Add smarter positional embedding
-        x = x.permute(1, 0, 2)  # NLD -> LND
+        'tokens': batch_size x max_seq_len x transformer_width
+        num_nodes + num_edges == num_non_pad_tokens
+
+        'image_embedding': batch_size x image_embedding_size
+        """
+
+        image_embedding = image_embedding.unsqueeze(1)
+        nodes = tokens[:, :num_nodes, :]
+        edges = tokens[:, num_nodes:num_non_pad_tokens, :]
+        padding = tokens[:, num_non_pad_tokens:, :]
+
+        if self.preembed_nodes:
+
+            nodes = self.image_embedder(
+                torch.cat(
+                    [nodes, image_embedding.repeat(1, num_nodes, 1)], dim=-1
+                ).permute(0, 2, 1)
+            ).permute(0, 2, 1)
+        else:
+            image_embedding = self.image_embedder(
+                image_embedding.permute(0, 2, 1)
+            ).permute(0, 2, 1)
+
+        nodes = nodes + self.positional_embeddings[NODES]
+        edges = edges + self.positional_embeddings[EDGES]
+
+        if self.preembed_nodes:
+            x = torch.cat(
+                [nodes, edges, padding],
+                dim=1,
+            )
+        else:
+            image_embedding = (
+                image_embedding + self.positional_embeddings[IMAGE_EMBEDDING]
+            )
+            x = torch.cat(
+                [
+                    image_embedding,
+                    nodes,
+                    edges,
+                    padding,
+                ],
+                dim=1,
+            )
+            x = x[:, : self.context_length, :]
         x = self.image_transformer(
             x, mask=image_attention_mask.repeat(self.transformer_heads, 1, 1)
         )
-        x = x.permute(1, 0, 2)  # LND -> NLD
         x = self.ln_final(x)
 
         # x.shape = [batch_size, n_ctx, transformer.width]
@@ -154,3 +225,20 @@ class VisionLanguageEncoder(nn.Module):
         text_embeddings = self.text_projection(text_features)
 
         return image_embeddings, text_embeddings
+
+
+if __name__ == "__main__":
+    vle = VisionLanguageEncoder(16, 16, 16, 8, 6, 32, preembed_nodes=False)
+    num_nodes = 5
+    num_non_pad_tokens = 15
+    tokens = torch.randn(5, 77, 16)
+    image_embedding = torch.randn(5, 32)
+    print(
+        vle.encode_image(
+            tokens,
+            image_embedding,
+            num_non_pad_tokens,
+            num_nodes,
+            torch.randn(5, 77, 77),
+        ).shape
+    )
